06e95b6da2185b2d9f8fea2f7580f37d6e01b034
naziatabassum777 <naziatabassum21795@gmail.com>
2020-09-21 01:02:45 +0600

    house

---
+++ House/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from .models import House
+
+# Create your views here.
+def House_info(request):
+    Houses = House.objects.all()
+    print(Houses)
+    context = {
+        "Houses": Houses
+    }
+    return render(request,'House/House_info.html',context)
