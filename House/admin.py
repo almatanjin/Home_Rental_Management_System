6004dd9af6316df444af2bf62f203a5b1b7e4f3d
naharshimu <68416206+naharshimu@users.noreply.github.com>
2020-09-21 14:14:50 +0600

    Address

--- House/admin.py
+++ House/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from . models import House
+from . models import House,Address
 
 # Register your models here.
-admin.site.register(House)
+admin.site.register([House,Address])
--- House/models.py
+++ House/models.py
@@ -2,7 +2,20 @@ from django.db import models
 
 
 # Create your models here.
+class Address(models.Model):
+    city=models.CharField(max_length=25)
+    house_no= models.IntegerField(max_length=10)
+    road_no = models.IntegerField(max_length=10)
+    area= models.CharField(max_length=25)
+
+    def __str__(self):
+        return str (self.road_no) + " " + self.area + " ," + self.city
+
+
+
+
 class House(models.Model):
-    room_no =  models.IntegerField(max_length=10)
     size_in_sqfeet = models.IntegerField(max_length=10)
     rent = models.IntegerField(max_length=25)
+    room_no = models.IntegerField(max_length=10)
+    address=models.ForeignKey(Address,on_delete=models.CASCADE,default=1)
