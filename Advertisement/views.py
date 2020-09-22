41c635cf59bc573e6bf633e53a56883c5331bb79
Alma Tanjin <almatajin.101999@gmail.com>
2020-09-22 20:20:51 +0600

    advertisment showpage

--- Advertisement/views.py
+++ Advertisement/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 from .advertisment_models import Advertisement
 # Create your views here.
 def advertisementinfo(request):
-    return render(request,'advertisement/advertisement_info.html')
+    advertisment = Advertisement.objects.all()
+    print(advertisment)
+    context = {
+        "Advertisment": advertisment
+    }
+    return render(request,'advertisement/advertisement_info.html',context)
