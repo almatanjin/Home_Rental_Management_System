2922ac673e67d010e3d0fb63a6977135cb7f92ef
Alma Tanjin <almatajin.101999@gmail.com>
2020-10-07 00:34:00 +0600

    media

--- Landlord/landlord_models.py
+++ Landlord/landlord_models.py
@@ -1,11 +1,13 @@
 from django.db import models
 
 
+
 # Create your models here.
 class Landlord(models.Model):
     name = models.CharField(max_length=100)
     Contact_number = models.IntegerField()
     email = models.EmailField(max_length=100)
+
 
     def __str__(self):
         return self.name
